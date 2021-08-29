23784044a866295fb52c9ace0e4cee984fa8857f
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 23:35:19 +0530

    Add project files.

---
+++ ConsoleApp_print_pattern_byhalfnum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp_print_pattern_byhalfnum
+{
+    class PatternNo
+    {
+        public void pattern(int num)
+        {
+            for (int i = 0 ; i <=num ; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    
+                    Console.Write(+ j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                PatternNo obj = new PatternNo();
+                Console.Write("Enter the any number : ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                obj.pattern(n);
+        }
+    }
+}
